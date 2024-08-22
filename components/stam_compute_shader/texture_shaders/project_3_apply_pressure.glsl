47da6e22694239d66b249c66d25a73b6f88f5a79
Joe Booth <joe@joebooth.com>
2024-08-21 22:52:47 -0700

    SMIT apply presure

--- components/stam_compute_shader/texture_shaders/project_3_apply_pressure.glsl
+++ components/stam_compute_shader/texture_shaders/project_3_apply_pressure.glsl
@@ -29,9 +29,10 @@ void main() {
     uint idy = gl_GlobalInvocationID.y;
     ivec2 cell = ivec2(idx, idy);
 
-    if (texelFetch(s, cell, 0).r == 0.0 || idx == 0 || idx >= consts.numX - 1 || idy == 0 || idy >= consts.numY - 1) {
-        return;
-    }
+    // if (texelFetch(s, cell, 0).r == 0.0 || idx == 0 || idx >= consts.numX - 1 || idy == 0 || idy >= consts.numY - 1) {
+    //     return;
+    // }
+    bool skip = (texelFetch(s, cell, 0).r == 0.0);
 
     ivec2 cell_l = ivec2(idx-1, idy);
     ivec2 cell_r = ivec2(idx+1, idy);
@@ -46,6 +47,9 @@ void main() {
 
     u_val -= 0.5 * (texelFetch(p, cell_r, 0).r - texelFetch(p, cell_l, 0).r) / _h;
     v_val -= 0.5 * (texelFetch(p, cell_d, 0).r - texelFetch(p, cell_u, 0).r) / _h;
+
+    u_val = skip ? 0 : u_val;
+    v_val = skip ? 0 : v_val;
 
     imageStore(u, cell, vec4(u_val));
     imageStore(v, cell, vec4(v_val));
