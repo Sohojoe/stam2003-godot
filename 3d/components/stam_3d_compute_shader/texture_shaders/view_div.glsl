444352914cc49b6c556e386f18665d3ec72b500d
Joe Booth <joe@joebooth.com>
2024-08-27 22:22:46 -0700

    fix: view div

--- 3d/components/stam_3d_compute_shader/texture_shaders/view_div.glsl
+++ 3d/components/stam_3d_compute_shader/texture_shaders/view_div.glsl
@@ -1,7 +1,7 @@
 #[compute]
 #version 450
 
-layout(local_size_x = 16, local_size_y = 1, local_size_z = 1) in;
+layout(local_size_x = 16, local_size_y = 16, local_size_z = 1) in;
 
 // --- Begin Shared Buffer Definition
 layout(set = 0, binding = 0, std430) readonly buffer ConstBuffer {
@@ -16,7 +16,7 @@ layout(set = 0, binding = 0, std430) readonly buffer ConstBuffer {
 } consts;
 
 
-layout(set = 0, binding = 1) uniform sampler2D div;
+layout(set = 0, binding = 1) uniform sampler3D div;
 layout(set = 0, binding = 2,rgba32f) writeonly uniform image2D output_image;
 
 
@@ -27,22 +27,30 @@ layout(push_constant, std430) uniform Params {
 } pc;
 
 void main() {
-    ivec2 cell = ivec2(gl_GlobalInvocationID.xy);
-    vec2 texelSize = 1.0 / vec2(consts.viewX, consts.viewY);
-    vec2 UV = (vec2(cell) + 0.5) * texelSize;
-
-    float div = texture(div, UV).r; 
-    div = min(div / pc.color_scale, 1.);
+    ivec3 cell = ivec3(gl_GlobalInvocationID.xyz);
+    vec3 texelSize = 1.0 / vec3(consts.viewX, consts.viewY, consts.viewZ);
+    vec3 UVW = (vec3(cell) + 0.5) * texelSize;
+    vec3 input_texelSize = 1.0 / vec3(consts.numX, consts.numY, consts.numZ);
+    UVW.z = (consts.numZ - 1 + 0.5) * input_texelSize.z;
 
     float r = 0.;
     float g = 0.;
     float b = 0.;
+    vec4 finalColor = vec4(0., 0., 0., 1.);
+    vec4 add_color = vec4(0.0);
 
-    if (div < 0.0) {
-        r = 0.0; g = -div; b = -div;
-    } else {
-        r = div; g = div; b = 0.0;  //  positive divergence
+    for (int z = 0; z < int(consts.numZ) - 1; z++) {
+        float div = texture(div, UVW).r; 
+        div = min(div / pc.color_scale, 1.);
+
+        if (div < 0.0) {
+            r = 0.0; g = -div; b = -div;
+        } else {
+            r = div; g = 0.0; b = 0.0;  //  positive divergence
+        }
+        finalColor.rgb += vec3(r, g, b);
+        UVW.z -= input_texelSize.z;
     }
-    vec4 color = vec4(r, g, b, 1.0);
-    imageStore(output_image, cell, color);
+    finalColor = clamp(finalColor, 0.0, 1.0);
+    imageStore(output_image, cell.xy, finalColor);
 }
